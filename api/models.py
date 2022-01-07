8447bf911b0ad45051e70dc1e0b08bd3e32ad5e0
Samson Okubanjo <samsonokubanjo@outlook.com>
2022-01-07 11:07:44 +0100

    refactored search function

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from .utils import get_collection
+from django.conf import settings
 
-# Create your models here.
+
+def search_product_title(query, offset, limit):
+    collection = get_collection(settings.GARMENT_COLLECTION)
+    neededFields = {'image_urls': 1, 'product_title': 1, 'url': 1,
+                    'product_description': 1, 'currency_code': 1, 'price': 1}
+    total = collection.count_documents(query)
+    results = collection.find(query, neededFields).skip(
+        offset).limit(limit)
+    return total, results
--- api/views.py
+++ api/views.py
@@ -1,26 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .utils import get_collection
-from django.conf import settings
+from .models import search_product_title
 
 
 def search(request):
-    collection = get_collection(settings.GARMENT_COLLECTION)
     searchQuery = request.GET.get('q', '')
     per_page = 20
     page = int(request.GET.get('p', '1'))
     offset = (page - 1) * per_page
-    neededFields = {'image_urls': 1, 'product_title': 1, 'url': 1,
-                    'product_description': 1, 'currency_code': 1, 'price': 1}
     if searchQuery == '':
-        total = collection.count_documents({})
-        results = collection.find({}, neededFields).skip(
-            offset).limit(per_page)
+        total, results = search_product_title({}, offset, per_page)
     else:
         queryDic = {'product_title':  {'$regex': searchQuery, '$options': 'i'}}
-        total = collection.count_documents(queryDic)
-        results = collection.find(queryDic, neededFields).skip(
-            offset).limit(per_page)
+        total, results = search_product_title(queryDic, offset, per_page)
     resultsList = []
     for row in results:
         del row['_id']
